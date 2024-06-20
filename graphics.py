9ceccbfb75e1814ddad477637aa63f66e3339bb6
PedroPiveta <ppivetabarrotti208@gmail.com>
2024-06-20 15:46:26 -0300

    basic initial functions

---
+++ graphics.py
@@ -0,0 +1,75 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__running = True
+        while self.__running:
+            self.redraw()
+        print("Window closed...")
+
+    def close(self):
+        self.__running = False
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.point1.x, self.point1.y, self.point2.x, self.point2.y,
+            fill=fill_color, width=2
+        )
+
+class Cell:
+    def __init__(self, win, x1, y1, x2, y2):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+        self._win = win
+
+    def draw(self):
+        if self.has_left_wall:
+            self._win.draw_line(Line(Point(self._x1, self._y1), Point(self._x1, self._y2)))
+        if self.has_right_wall:
+            self._win.draw_line(Line(Point(self._x2, self._y1), Point(self._x2, self._y2)))
+        if self.has_top_wall:
+            self._win.draw_line(Line(Point(self._x1, self._y1), Point(self._x2, self._y1)))
+        if self.has_bottom_wall:
+            self._win.draw_line(Line(Point(self._x1, self._y2), Point(self._x2, self._y2)))
+
+    def draw_move(self, to_cell, undo=False):
+        fill_color = "gray" if undo == True else "red"
+        from_x_center = (self._x1+ self._x2)/2 
+        from_y_center = (self._y1+ self._y2)/2
+        
+        to_x_center = (to_cell._x1 + to_cell._x2)/2
+        to_y_center = (to_cell._y1 + to_cell._y2)/2
+
+        self._win.draw_line(Line(Point(from_x_center, from_y_center), Point(to_x_center, to_y_center)), fill_color)
+
---
+++ main.py
@@ -0,0 +1,33 @@
+from graphics import Window, Point, Line, Cell
+
+def main():
+    win = Window(800, 600)
+
+    cell1 = Cell(win, 50, 50, 150, 150)
+    cell1.draw()  # Draw cell1
+    
+    cell2 = Cell(win, 200, 50, 300, 150)
+    cell2.has_left_wall = False
+    cell2.draw()  # Draw cell2 with no left wall
+
+    cell1.draw_move(cell2)
+    
+    cell3 = Cell(win, 350, 50, 450, 150)
+    cell3.has_top_wall = False
+    cell3.has_bottom_wall = False
+    cell3.draw()  # Draw cell3 with no top and bottom walls
+    
+    cell4 = Cell(win, 500, 50, 600, 150)
+    cell4.has_right_wall = False
+    cell4.draw()  # Draw cell4 with no right wall
+    
+    cell5 = Cell(win, 50, 200, 150, 300)
+    cell5.has_left_wall = False
+    cell5.has_right_wall = False
+    cell5.has_top_wall = False
+    cell5.has_bottom_wall = False
+    cell5.draw()  # Draw cell5 with no walls
+
+    win.wait_for_close()
+main()
+
